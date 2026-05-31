97d27b8b04a08f2c74d0235619904416fc644375
Gia Nghi Tran <gtra0015@student.monash.edu>
2026-05-31 16:17:21 +0700

    Update and finalise section 1

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(console.log)
     .catch(console.error);
 
-  vegaEmbed("#fuel_bar", "js/fuel_bar.vg.json")
-    .then(console.log)
-    .catch(console.error);
+  vegaEmbed(
+    "#fuelCycleHeatmap",
+    "js/fuel_cycles.vg.json",
+    { actions: false }
+  ).catch(console.error);
+
+  vegaEmbed(
+    "#fuelCompareFacetplot",
+    "js/fuel_compare.vg.json",
+    { actions: false }
+  ).then(result => {
+    console.log("Facet plot loaded");
+  }).catch(err => {
+    console.error("Vega embed error:", err);
+  });
+
+  vegaEmbed("#fuel_ranked_bar", "js/fuel_ranked_bar.vg.json", {
+  actions: false
+  }).catch(console.error);
 
 });
