9802102234dc5de0dca56370f1f7cbf87bda41eb
Gia Nghi Tran <gtra0015@student.monash.edu>
2026-05-31 11:02:55 +0700

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+vegaEmbed("#fuel_map", "js/fuel_map.vg.json");
+// vegaEmbed("#fuel_line", "js/fuel_line.vg.json");
+
+// vegaEmbed("#policy_bar", "js/policy_bar.vg.json");
+// vegaEmbed("#fare_policy", "js/fare_policy.vg.json");
+
+// vegaEmbed("#patronage_line", "js/patronage_line.vg.json");
+// vegaEmbed("#recovery_area", "js/recovery_area.vg.json");
+// vegaEmbed("#train_tram_bus_bar", "js/train_tram_bus_bar.vg.json");
+
+// vegaEmbed("#city_bubble", "js/city_bubble.vg.json");
+// vegaEmbed("#fuel_vs_patronage", "js/fuel_vs_patronage.vg.json");
+// vegaEmbed("#mode_share", "js/mode_share.vg.json");
+// vegaEmbed("#regional_vs_metro", "js/regional_vs_metro.vg.json");
+
+// vegaEmbed("#state_compare", "js/state_compare.vg.json");
+// vegaEmbed("#ridership_heatmap", "js/ridership_heatmap.vg.json");
+// vegaEmbed("#slopegraph", "js/slopegraph.vg.json");
+// vegaEmbed("#final_summary", "js/final_summary.vg.json");
